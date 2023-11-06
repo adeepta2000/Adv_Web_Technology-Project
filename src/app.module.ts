27a5e30819c6abd5dce319c2afc47fc9cc778585
Sakib-Ul-Ahsan <sakib.ahsan.35@gmail.com>
2023-11-06 12:32:56 +0600

    Added all the required folders and files agent

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,15 @@ import { AppService } from './app.service';
 import { AdminModule } from './Admin/AdminModule.module';
 import { EmployeeModule } from './Employee/EmployeeModule.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AgentModule } from './Agent/agent.module';
 
 @Module({
-  imports: [AdminModule, EmployeeModule,TypeOrmModule.forRoot(
+  imports: [AdminModule, EmployeeModule, AgentModule, TypeOrmModule.forRoot(
     { type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: '12345',
+    password: '1234',
     database: 'Tourism_Management_System',
     autoLoadEntities: true,
     synchronize: true,
